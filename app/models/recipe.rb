bb53d19bdab4315ee6668b9dc656eef24d2de02b
shannon-nabors <shannon.nabors@gmail.com>
2019-02-05 12:59:52 -0500

    done

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,10 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+  accepts_nested_attributes_for :ingredients
+  before_save :remove_blank_ingredients
+
+  def remove_blank_ingredients
+    self.ingredients = self.ingredients.select {|i| !i.name.blank? && !i.quantity.blank?}
+  end
+  
 end
