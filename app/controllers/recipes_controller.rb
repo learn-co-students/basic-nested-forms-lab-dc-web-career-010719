7cd6c202e38ce8272bd01631fd3c6961cc741efd
canikwe <c.anikwe@gmail.com>
2019-02-05 14:12:34 -0500

    created nested form

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,6 @@
 class RecipesController < ApplicationController
+
+  before_action :get_recipe, only: [:show]
   def show
     @recipe = Recipe.find(params[:id])
   end
@@ -9,8 +11,30 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @ingredients = @recipe.ingredients.build(name: 'First')
+    @ingredients = @recipe.ingredients.build(name: 'Second')
   end
 
   def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe
+    else render :new
+    end
   end
+
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(
+      :title,
+      ingredients_attributes:
+        [:name, :quantity, :recipe_id]
+      )
+  end
+
+  def get_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
 end
